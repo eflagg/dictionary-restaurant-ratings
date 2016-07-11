a6301a111a5afb6dae33605cc5f57b0970d57fb6
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 16:37:19 -0700

    Created alphabetize function, prints name and rating (unsorted)

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,17 @@
 # your code goes here
+def alphabetize(filename):
+    file = open(filename)
+    restaurants = {}
+    line = []
+    
+    for tup in file:
+        line = tup.rstrip()
+        line = line.split(":")
+
+        for item in line:
+            restaurants[line[0]] = line[1]
+
+    for restaurant_name, rating in restaurants.iteritems():
+        print "%s is rated at %s." % (restaurant_name, rating)
+
+alphabetize('scores.txt')
