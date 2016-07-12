7b45376722f0bef42c8c2f0cb2b5ea0863e49b89
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:45:41 -0700

    Minor changes to styling, removed redundancy

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,6 +1,7 @@
 # your code goes here
 import random
 
+
 def alphabetize(filename):
     """Alphabetizes list of restaurants and ratings
 
@@ -10,32 +11,29 @@ def alphabetize(filename):
     username = raw_input("Hi, what's your name? ")
     print "Hi %s!" % (username)
 
-    file = open(filename)
+    open_file = open(filename)
     restaurants = {}
-    line = []
-    
-    # loops through textfile, strips any hanging spaces, splits into a list based on ':'
-    for tup in file:
-        line = tup.rstrip()
-        line = line.split(":")
 
-        # loops through each list created from the outer for loop and inserts it into 
-        # the dictionary
-        for item in line:
-            restaurants[line[0]] = line[1]
+    # loops through textfile, strips any hanging spaces, splits into a list based on ':'
+    # Inserts restaurant name and rating into the dictionary
+    for line in open_file:
+        line = line.rstrip()
+        line_as_list = line.split(":")
+
+        restaurants[line_as_list[0]] = line_as_list[1]
 
     #Generates a random restaurant, asks user for rating and changes rating within the 
     #dictionary. Does this until user types 'q' or 'Q'
     while True: 
-        random_restaurant_tuple = random.choice(restaurants.items())   
+        random_restaurant_key = random.choice(restaurants.keys())   
         print "Let's look at a sample restaurant!"     
-        print random_restaurant_tuple[0] + ": " + random_restaurant_tuple[1]
+        print random_restaurant_key + ": " + restaurants[random_restaurant_key]
 
         new_rating = raw_input("What should the new rating be? ")
-        restaurants[random_restaurant_tuple[0]] = new_rating
         if new_rating in ['q','Q']:
             break
         else:
+            restaurants[random_restaurant_key] = new_rating
             print "Success! You successfully changed the restaurant rating!"
 
     #Asks user to input another restaurant with a rating and adds to dictionary
